8ef1114ad2a9f8703145931da371cc29ef0394bc
Zaharuddin <muhammad.zaharuddin@accenture.com>
2020-03-10 10:29:40 +0800

    fix repo test

--- src/test/java/com/example/unittesting/repository/RepositoryTest.java
+++ src/test/java/com/example/unittesting/repository/RepositoryTest.java
@@ -3,8 +3,7 @@ package com.example.unittesting.repository;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,41 +12,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.example.unittesting.models.Pet;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 public class RepositoryTest {
-
-    @Autowired
-    private TestEntityManager testEntityManager;
 
     @Autowired
     private PetRepository petRepository;
 
-//    @Before
-//    public void setUp() {
-//        Pet pet1 = new Pet(100, "cat", "male", 2);
-//        Pet pet2 = new Pet(101, "dog", "male", 5);
-//        Pet pet3 = new Pet(102, "rabbit", "female", 3);
-//        petRepository.addPet(pet1);
-//        petRepository.addPet(pet2);
-//        petRepository.addPet(pet3);
-//        testEntityManager.persist(pet1);
-//        testEntityManager.persist(pet2);
-//        testEntityManager.persist(pet3);
-//    }
-
-//    @After
-//    public void tearDown() throws Exception {
-//        petRepository.deletePet(100);
-//        petRepository.deletePet(101);
-//        petRepository.deletePet(102);
-//    }
-
     @Test
     public void testSavePet() {
-        Pet pet1 = new Pet(100, "cat", "male", 2);
+        Pet pet1 = new Pet(1,"dog", "female", 5);
         petRepository.addPet(pet1);
-        Pet pet2 = petRepository.getPet(100);
-        assertNotNull(pet1);
+        Pet pet2 = petRepository.getPet(pet1.getId());
         assertEquals(pet2.getId(), pet1.getId());
         assertEquals(pet2.getType(), pet1.getType());
         assertEquals(pet2.getGender(), pet1.getGender());
@@ -56,31 +31,44 @@ public class RepositoryTest {
 
     @Test
     public void testFindAll() {
+        Pet pet1 = new Pet(2,"rabbit", "female", 3);
+        petRepository.addPet(pet1);
         List<Pet> pets = petRepository.getAllPet();
-//        assertEquals(3, pets.size());
-        assertThat(pets).hasSize(3);
+        assertThat(pets).hasSize(2);
     }
 
     @Test
-    public void testFindById() {
-        Pet pet1 = new Pet(100, "cat", "male", 2);
-        testEntityManager.persist(pet1);
-        Pet optPet = petRepository.getPet(100);
-        assertThat(optPet).isNotNull();
+    public void testFindById_NotFound() {
+        Pet pet1 = petRepository.getPet(1000);
+        assertNull(pet1);
     }
-//
-//    @Test
-//    public void testFindById_Basic() {
-//        Pet pet1 = new Pet(100, "cat", "male", 2);
-//        petRepository.addPet(pet1);
-//        Pet optPet = petRepository.getPet(100);
-//        assertNotNull(optPet);
-//    }
-//
-//    @Test
-//    public void testFindById_NotFound() {
-//        Pet optPet = petRepository.getPet(1000);
-//        assertNull(optPet);
-//    }
+
+    @Test
+    public void testUpdatePet() {
+        Pet pet1 = new Pet(3,"cat", "male", 2);
+        petRepository.addPet(pet1);
+        Pet pet2 = petRepository.getPet(pet1.getId());
+        assertEquals(pet2.getType(), pet1.getType());
+        assertEquals(pet2.getGender(), pet1.getGender());
+        assertEquals(pet2.getAge(), pet1.getAge());
+        Pet pet3 = new Pet(pet2.getId(), "hamster", "female", 4);
+        petRepository.updatePet(pet3.getId(), pet3);
+        Pet pet4 = petRepository.getPet(pet3.getId());
+        assertEquals(pet4.getType(), pet3.getType());
+        assertEquals(pet4.getGender(), pet3.getGender());
+        assertEquals(pet4.getAge(), pet3.getAge());
+
+    }
+
+    @Test
+    public void testDeletePetById() {
+        Pet pet1 = petRepository.getPet(2);
+        assertNotNull(pet1);
+        petRepository.deletePet(pet1.getId());
+        Pet pet2 = petRepository.getPet(2);
+        assertNull(pet2);
+    }
+
+
 
 }
